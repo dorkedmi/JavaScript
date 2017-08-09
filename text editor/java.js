2d2c533ee3450fdd20a0478f02618f1b9bcabec4
dorkedmi <dorkedmy@gmail.com>
2017-08-09 13:54:08 +0300

    cv+json(on server) and text editor task

--- text editor/java.js
+++ text editor/java.js
@@ -1,18 +1,28 @@
-$(function() {
-
-  var counter = 0;
-
-  $('#title').html(localStorage['title']);
-  $('#content').html(localStorage['text']);
-
-   setInterval(function() {
-        //Update counter
-        counter++;
-        $('#counter').html(counter);
-
-        //Save
-        localStorage['title'] = $('#title').html();
-        localStorage['text'] = $('#content').html();
-   }, 1000);
-
-});
+$(function(){
+    function getColor() {
+      return '#' + Math.random().toString(16).slice(2, 8);
+    }
+    
+    $('#lists').html(localStorage['lists']);
+    
+  $(window).keypress(function(e){
+        if(e.keyCode==13)
+            {
+                newitem();
+                localStorage['lists'] = $('#lists').html();
+            }
+    });
+    
+    $('ul').on('click','li',function(e){
+        $(e.target).remove();
+        localStorage['lists'] = $('#lists').html();
+    });
+    
+    function newitem(){ 
+        var item = $('input').val();
+        var color = getColor();
+        $('ul').append('<li class="lists" style="color:'+ color +';">'+ item +'</li>');
+        $('input').val('');
+    }
+    
+  });
